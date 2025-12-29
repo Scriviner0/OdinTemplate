c41f8cd7ae82bead68e280835334c7775fc8e074
MUHAMMED SAHAL PV <182391491+Scriviner0@users.noreply.github.com>
2025-12-29 18:53:45 +0530

    Refactor webpack.dev.js to use webpack-merge

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,13 @@
-const path = require("path")
-const HtmlWebpackPlugin = require("html-webpack-plugin")
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
 
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "eval-source-map",
 
-module.exports = {
-    mode: "development",
-    devtool: "eval-source-map",
-    devServer: {
-	watchFiles: ["./src/template.html"],
-    },
-}
+  devServer: {
+    static: "./dist",
+    watchFiles: ["./src/index.html"],
+    open: true,
+  },
+});
